c18a4712c011ed458689a21dd6acffb3913142aa
hi-jennie <jennie.wang2024@gmail.com>
2025-10-24 17:21:12 +0200

    Add dish management functionality with dish and flavor persistence

---
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -0,0 +1,45 @@
+package com.sky.service.impl;
+
+import com.sky.dto.DishDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
+import com.sky.mapper.DishMapper;
+import com.sky.service.DishService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class DishServiceImpl implements DishService {
+
+    @Autowired
+    private DishMapper dishMapper;
+
+    @Autowired
+    private DishFlavorMapper dishFlavorMapper;
+
+    @Override
+    // It tells Spring that all the database operations inside this method should succeed or fail together.
+    @Transactional
+    public void saveWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        // insert 1 row into dish table
+        dishMapper.insert(dish);
+
+        // Save flavors into dish_flavor table
+        // this is done by MyBatis useGeneratedKeys="true" keyProperty="id")
+        Long dishId = dish.getId();
+
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        if (flavors != null && flavors.size() > 0) {
+            flavors.forEach(dishFlavor -> dishFlavor.setDishId(dishId));
+            // insert multiple rows into dish_flavor table
+            dishFlavorMapper.insertBatch(flavors);
+        }
+    }
+}
